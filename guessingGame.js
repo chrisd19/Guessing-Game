147bf7e45dd796d84da68419bf25ccef5963a94c
Chris DeMoll <demoll.chris@gmail.com>
2016-05-19 23:53:19 -0400

    fully functional

--- guessingGame.js
+++ guessingGame.js
@@ -2,12 +2,22 @@
 // try to elminate these global variables in your project, these are here just to start.
 
 // wrap entire module as a function...can still use variables
+// before doing so, need to add event listeners in place of onclick
+// attributes in the html
 
 var playersGuess,
     winningNumber = generateWinningNumber(),
     previousGuesses = [],
-    guessesTaken = 0; 
+    guessesRemaining = 5,
+    hintUsed = false; 
 
+$(document).ready(function() {
+	$('#guess').on('keyup', function(key) {
+		if (key.which === 13) {
+			playersGuessSubmission();
+		}
+	});
+});
 
 /* **** Guessing Game Functions **** */
 
@@ -21,49 +31,126 @@ function generateWinningNumber(){
 
 function playersGuessSubmission(){
 	playersGuess = +(document.getElementById('guess').value);
-	$('#guess').val("");
-	checkGuess();
+	if (typeof playersGuess === 'number' && 
+		playersGuess >= 1 && playersGuess <= 100) {
+		$('#guess').val("");
+		checkGuess();
+
+		var remainingUpdate = "Guesses Remaining: " + guessesRemaining;
+		$('#remaining').text(remainingUpdate);
+	}
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
+	var diff = winningNumber - playersGuess;
+	var side = diff < 0 ? "Lower" : "Higher";
+	var absDiff = Math.abs(diff);
+	var dist;
+
+	if (absDiff < 5) { dist = "SCALDING"; }
+	else if (absDiff < 10) { dist = "HOT"; }
+	else if (absDiff < 25) { dist = "TEPID"; }
+	else if (absDiff < 50) { dist = "COLD"; }
+	else { dist = "FREEZING"; }
+
+	return dist + ": Think " + side; 
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	var message;
+	var message, guidance;
 
 	if (playersGuess === winningNumber) {
-		guessesTaken ++;
-		message = "You Win!";
+		guessesRemaining --;
+		winner();
 	} else if (previousGuesses.indexOf(playersGuess) > -1) {
 		message = "Already Guessed";
+		guidance = lowerOrHigher();
+	} else if (guessesRemaining === 1) {
+		guessesRemaining --;
+		loser();
 	} else {
-		guessesTaken ++;
+		guessesRemaining --;
 		message = "Try Again";
+		guidance = lowerOrHigher();
 	}
 
 	previousGuesses.push(playersGuess);
 
-	$('#feedback').text(function() {
-		return message;
-	});
-	$('#feedback').slideDown();
+	if (message !== undefined) {
+		$('.feedback').children('#message').text(function() {
+			return message;
+		}).slideDown();
+		if (previousGuesses.length > 0) {
+			$('#guidance').css('display', 'none');
+		}
+		$('#guidance').text(function() {
+			return guidance;
+		}).slideDown();
+	}
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
 
 function provideHint(){
-	// add code here
+	if (hintUsed) {
+		$('#not-allowed').slideDown().delay(1500).slideUp();
+	} else {
+		hintUsed = true;
+		var hintMessage = "The answer is one of these numbers: ";
+		var hintNums = [];
+		var index = Math.floor(Math.random() * (guessesRemaining + 1));
+
+		for (var i = 0; i < guessesRemaining + 1; i++) {
+			hintNums.push(generateWinningNumber());
+		}
+
+		hintNums[index] = winningNumber;
+		hintMessage += hintNums.join(", ");
+
+		$('#hint').text(function() {
+			return hintMessage;
+		}).slideDown();
+	}
 }
 
 // Allow the "Player" to Play Again
 
 function playAgain(){
-	// add code here
+	playersGuess = undefined;
+	winningNumber = generateWinningNumber(),
+    previousGuesses = [],
+    guessesRemaining = 5,
+    hintUsed = false;
+
+    $('#play-again').fadeOut(200);
+    $('#win-or-lose').delay(200).fadeOut();
+    $('.well').delay(800).fadeIn();
+    $('#message').slideUp();
+    $('#guidance').slideUp();
+    $('#hint').slideUp();
+    $('#remaining').text(function() {
+    	return "Guesses Remaining: 5";
+    });
+}
+
+function winner() {
+	$('.well').fadeOut();
+	$('#win-or-lose').text(function() {
+		return "You Win!";
+	}).css('color', 'hsla(115, 100%, 60%, 1)').delay(600).fadeIn(2000);
+	$('#play-again').delay(2000).fadeIn();
+}
+
+function loser() {
+	$('.well').fadeOut();
+	$('#win-or-lose').text(function() {
+		return "You Lose!";
+	}).css('color', 'hsla(0, 100%, 52%, 1)').delay(600).fadeIn(2000);
+	$('#play-again').delay(2000).fadeIn();
 }
 
 
